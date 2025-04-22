13ce2fb01509c41f3e8f30b2a4dd49fb7fca1f36
MH TOUFIK <toufik@sopnolikhi.com>
2025-04-22 17:05:00 +0000

    Fix the problem to run the project

---
+++ src/main/java/com/skyjourney/controllers/ContactController.java
@@ -0,0 +1,28 @@
+package com.skyjourney.controllers;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class ContactController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Set encoding in case you're using UTF-8 (important!)
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+
+        // Get form parameters
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String subject = req.getParameter("subject");
+        String message = req.getParameter("message");
+
+        // Basic output response
+        resp.getWriter().println("<h2>Contact Message Received</h2>");
+        resp.getWriter().println("<p><strong>Name:</strong> " + name + "</p>");
+        resp.getWriter().println("<p><strong>Email:</strong> " + email + "</p>");
+        resp.getWriter().println("<p><strong>Subject:</strong> " + subject + "</p>");
+        resp.getWriter().println("<p><strong>Message:</strong> " + message + "</p>");
+    }
+}
--- src/main/java/com/skyjourney/models/FlightInfo.java
+++ src/main/java/com/skyjourney/models/FlightInfo.java
@@ -1,4 +1,4 @@
-package models;
+package com.skyjourney.models;
 
 public class FlightInfo {
     public String flight_name = "N/A";
@@ -8,8 +8,8 @@ public class FlightInfo {
     public String starting_destination = "N/A";
     public String ending_destination = "N/A";
 
-    public FlightInfo(String flight_name, int flight_number, int ticket_price, String starting_destination, String ending_destination)
-    {
+    public FlightInfo(String flight_name, int flight_number, int ticket_price, String starting_destination,
+            String ending_destination) {
         this.flight_name = flight_name;
         this.flight_number = flight_number;
         this.ticket_price = ticket_price;
