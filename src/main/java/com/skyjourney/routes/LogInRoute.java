3fa6ed81995bc43bcbb65e4c6ff6fcde20fd48b7
tishi015 <sufiasultana1952@gmail.com>
2025-06-24 10:08:54 +0000

    Login feature add dynamicaly

--- src/main/java/com/skyjourney/routes/LogInRoute.java
+++ src/main/java/com/skyjourney/routes/LogInRoute.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.skyjourney.controllers.UserController;
+
 @WebServlet("/login")
 public class LogInRoute extends HttpServlet {
 
@@ -16,6 +18,8 @@ public class LogInRoute extends HttpServlet {
         String emailOrPhone = req.getParameter("email");
         String password = req.getParameter("password");
 
+        // System.out.println(emailOrPhone);
+        // System.out.println(password);
         /**
          * TODO: Implement login functionality
          * Step 1: Create a model class to represent user credentials and validation
@@ -29,7 +33,15 @@ public class LogInRoute extends HttpServlet {
          * Step 5: Must be implement this part @Author Nasfim
          */
 
-        resp.sendRedirect("signin.jsp?success=1&name=MH TOUFIK&email=email@example.com&token=asdfhasjdfhsagd");
+        UserController user = new UserController();
+        
+        if(user.login(emailOrPhone, password)){
+            resp.sendRedirect("signin.jsp?success=1&name=MH TOUFIK&email=email@example.com&token=asdfhasjdfhsagd");
+        }
+        else{
+            resp.sendRedirect("signin.jsp?error=1");
+        }
+
     }
 
 }
